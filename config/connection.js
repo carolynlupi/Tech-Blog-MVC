56cabc5a5f34e6d22dcf34e1c780ac730fc6a028
carolynlupi <karolynlupi@gmail.com>
2023-09-11 16:23:51 -0400

    Heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -4,17 +4,23 @@ require("dotenv").config();
 // Sequelize import
 const Sequelize = require("sequelize");
 
-// Allows for environmental variables to be used
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: "localhost",
-      port: 8889,
-      dialect: "mysql",
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+// Define a variable to hold the database connection information
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  // Use the JawsDB URL provided by Heroku for production
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // Use local database configuration for development
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 3306, // Use 3306 as the default MySQL port
+    dialect: "mysql",
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  });
+}
 
 // Sequelize export
 module.exports = sequelize;
