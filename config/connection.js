2859eb20389174939e0e494566f84e72cae9be31
carolynlupi <karolynlupi@gmail.com>
2023-09-11 19:51:23 -0400

    Bug fix with heroku

--- config/connection.js
+++ config/connection.js
@@ -4,17 +4,24 @@ require("dotenv").config();
 // Sequelize import
 const Sequelize = require("sequelize");
 
-// Allows for environmental variables to be used
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: "localhost",
-      port: 8889,
-      dialect: "mysql",
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+// Determine the host and port based on the environment
+const host = process.env.JAWSDB_URL ? 'your_jawsdb_host_here' : 'localhost';
+const port = process.env.JAWSDB_URL ? 3306 : 8889; // Use 3306 for JawsDB, 8889 for local development
+
+// Create Sequelize instance
+const sequelize = new Sequelize(
+  process.env.JAWSDB_URL || process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: host,
+    port: port,
+    dialect: "mysql",
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  }
+);
 
 // Sequelize export
 module.exports = sequelize;
