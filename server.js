190a0d3fe546f3c6674baba1568ae854df4466c4
carolynlupi <karolynlupi@gmail.com>
2023-09-12 21:45:38 -0400

    Test on bug 2

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const helpers = require("./utils/helpers");
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
+const baseUrl = process.env.BASE_URL || '';
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
